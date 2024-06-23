2970b6fcb2f05739509a1e15bcb78553e5202450
GalKalev <132823231+GalKalev@users.noreply.github.com>
2024-06-23 15:46:54 +0300

    added the function for users:id

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,33 @@
 var express = require('express');
+const users = require('../models/users');
 var router = express.Router();
+
+
+/* GET users:id listing. */
+router.get('/:id', async function(request,response){
+  try{
+  
+    const userId = request.params.id;
+
+    // Finding the user in the database with the given id
+    const user = await users.findOne({id : userId});
+
+    // Checking to see if user exists in the database
+    if(!user){
+      return response.status(404).json({error: "User not found"});
+    }
+
+    response.status(200).json(user);
+  }catch(error){
+    console.error(`error user id ${error}`);
+    response.status(500).json({error:"Server Error"})
+  }
+  
+});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('this is users page');
 });
 
 module.exports = router;
